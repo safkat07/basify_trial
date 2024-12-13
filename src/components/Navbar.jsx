8362465d8332c22362aeb1604dd28e9169fe1a48
Safkat Sharif Sadli <safkatsharif@gmail.com>
2024-12-13 19:31:44 +0600

    Navbar modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { User2Icon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="max-w-full flex justify-between items-center p-2 text-center bg-white/75 border-2 shadow-xl">
+    <div className="max-w-full sticky top-0 flex justify-between items-center p-2 text-center bg-white border-2 shadow-xl">
       <div></div>
       <h2 className="text-3xl font-medium">Admin Dashboard</h2>
       <div className="flex flex-col justify-center items-center">
